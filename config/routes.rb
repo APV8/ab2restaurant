e5ed878a89c0413ca9e1e5fa6aad521ca6698d90
APV8 <andreinaperezviv@gmail.com>
2017-06-05 23:32:15 +0200

    Not sure

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'home/contact'
 
   resources :users
+  resources :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
